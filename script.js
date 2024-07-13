b18eeff1f90f7ffcbf58a0a5cab1a454ad13613f
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-07-13 11:31:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function stringChop(str, size) {
   // your code here
+	if (!str) return [];
+  
+  // Initialize an array to store the chunks
+  let chunks = [];
+  
+  // Iterate through the string in steps of 'size'
+  for (let i = 0; i < str.length; i += size) {
+    // Extract a substring of length 'size'
+    chunks.push(str.substr(i, size));
+  }
+  
+  // Return the array of chunks
+  return chunks;
 }
 
 // Do not change the code below
